74989c969974d74cc18e08698af235acd6e71f81
Dave Bemiller <dbemiller@appnexus.com>
2017-09-18 13:25:41 -0400

    Used a more realistic, properly base-64-encoded key.

--- azure/authorization_test.go
+++ azure/authorization_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestValidSignature(t *testing.T) {
-	auth := newAuthorization("abc")
+	auth := newAuthorization("aGprc2NoNzc2MjdlZHVpSHVER1NIQ0pld3lhNzMyNjRlN2ReIyQmI25jc2Fr")
 	sometime := time.Unix(123, 345).UTC()
 	formatedTime := sometime.Format("Mon, 02 Jan 2006 15:04:05 GMT")
 
 	signature := auth.sign("POST", "docs", "/dbs/prebidcache/colls/cache/docs", formatedTime)
-	expected := "type%3Dmaster%26ver%3D1.0%26sig%3D%2FV6RDQKxak0tC5KFaPtcEAQOOp%2BMFppHYg7%2BRcVN5ec%3D"
+	expected := "type%3Dmaster%26ver%3D1.0%26sig%3Db3cssh4LYbBDNUIWqAIfIxgbwllUao1BpLwUI8TT%2FCo%3D"
 
 	if signature != expected {
 		t.Errorf("Bad signature. Expected: %s, Got: %s", expected, signature)
