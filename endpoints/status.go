1889376b8a58c630ab6ce590be4ef636fb8bd145
Duc Chau <duc.chau@gmail.com>
2017-10-11 14:45:11 -0700

    changing to StatusOK to support healthchecks

--- endpoints/status.go
+++ endpoints/status.go
@@ -1,12 +1,12 @@
 package endpoints
 
 import (
-	"net/http"
 	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
 func Status(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// We might want more logic here eventually... but for now, we're ok to serve more traffic as
 	// long as the server responds.
-	w.WriteHeader(http.StatusNoContent)
+	w.WriteHeader(http.StatusOK)
 }
