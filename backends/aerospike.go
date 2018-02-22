04b94364a8c7ab73227844bdb2deea57f59b7628
dbemiller <27972385+dbemiller@users.noreply.github.com>
2018-02-22 15:21:59 -0500

    Fixed a small bug in the aerospike client (#18)
    * Fixed a small bug in the aerospike client. This stops the app from crashing if the aerospike client is configured wrong.
    
    * Replaced some magic strings with consts.

--- backends/aerospike.go
+++ backends/aerospike.go
@@ -2,9 +2,14 @@ package backends
 
 import (
 	"context"
-	as "github.com/aerospike/aerospike-client-go"
+	"errors"
+
 	log "github.com/Sirupsen/logrus"
+	as "github.com/aerospike/aerospike-client-go"
 )
+
+const setName = "uuid"
+const binValue = "value"
 
 type AerospikeConfig struct {
 	host      string
@@ -31,24 +36,27 @@ func NewAerospikeBackend(config *AerospikeConfig) (*Aerospike, error) {
 }
 
 func (a *Aerospike) Get(ctx context.Context, key string) (string, error) {
-	asKey, err := as.NewKey(a.config.namespace, "uuid", key)
+	asKey, err := as.NewKey(a.config.namespace, setName, key)
 	if err != nil {
 		return "", err
 	}
-	rec, err := a.client.Get(nil, asKey)
+	rec, err := a.client.Get(nil, asKey, "value")
 	if err != nil {
 		return "", err
 	}
-	return rec.Bins["value"].(string), nil
+	if rec == nil {
+		return "", errors.New("client.Get returned a nil record. Is aerospike configured properly?")
+	}
+	return rec.Bins[binValue].(string), nil
 }
 
 func (a *Aerospike) Put(ctx context.Context, key string, value string) error {
-	asKey, err := as.NewKey(a.config.namespace, "uuid", key)
+	asKey, err := as.NewKey(a.config.namespace, setName, key)
 	if err != nil {
 		return err
 	}
 	bins := as.BinMap{
-		"value": value,
+		binValue: value,
 	}
 	err = a.client.Put(nil, asKey, bins)
 	if err != nil {
