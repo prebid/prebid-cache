2c5f24c2764c1202ea98547aac375467561d0257
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-05-02 10:09:40 -0400

    Added back in the cors and rate limiting middleware.

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/didip/tollbooth"
+	"github.com/didip/tollbooth/limiter"
 	"github.com/julienschmidt/httprouter"
+	"github.com/rs/cors"
 	"github.com/spf13/viper"
 
-	"github.com/didip/tollbooth/limiter"
 	"github.com/prebid/prebid-cache/backends"
 	backendDecorators "github.com/prebid/prebid-cache/backends/decorators"
 	"github.com/prebid/prebid-cache/compression"
@@ -22,7 +23,55 @@ import (
 	"github.com/prebid/prebid-cache/server"
 )
 
-func initRateLimter(next http.Handler, cfg config.RateLimiting) http.Handler {
+func main() {
+	cfg := config.NewConfig()
+	initLogging(cfg)
+	appMetrics := metrics.CreateMetrics()
+	backend := newBackend(cfg, appMetrics)
+	handler := newHandler(cfg, backend, appMetrics)
+	go appMetrics.Export(cfg.Metrics)
+	server.Listen(cfg, handler, appMetrics.Connections)
+}
+
+func initLogging(cfg config.Configuration) {
+	level, err := log.ParseLevel(string(cfg.Log.Level))
+	if err != nil {
+		log.Fatalf("Invalid logrus level: %v", err)
+	}
+	log.SetOutput(os.Stdout)
+	log.SetLevel(level)
+	log.Info("Log level set to: ", log.GetLevel())
+}
+
+func newBackend(cfg config.Configuration, appMetrics *metrics.Metrics) backends.Backend {
+	backend := backends.NewBackend(cfg.Backend)
+	if cfg.RequestLimits.MaxSize > 0 {
+		backend = backendDecorators.EnforceSizeLimit(backend, cfg.RequestLimits.MaxSize)
+	}
+	backend = backendDecorators.LogMetrics(backend, appMetrics)
+	if viper.GetString("compression.type") == "snappy" {
+		backend = compression.SnappyCompress(backend)
+	}
+	return backend
+}
+
+func newHandler(cfg config.Configuration, dataStore backends.Backend, appMetrics *metrics.Metrics) http.Handler {
+	router := httprouter.New()
+	router.GET("/status", endpoints.Status) // Determines whether the server is ready for more traffic.
+	router.POST("/cache", endpointDecorators.MonitorHttp(endpoints.NewPutHandler(dataStore, cfg.RequestLimits.MaxNumValues), appMetrics.Puts))
+	router.GET("/cache", endpointDecorators.MonitorHttp(endpoints.NewGetHandler(dataStore), appMetrics.Gets))
+
+	handler := handleCors(router)
+	handler = handleRateLimiting(handler, cfg.RateLimiting)
+	return handler
+}
+
+func handleCors(handler http.Handler) http.Handler {
+	coresCfg := cors.New(cors.Options{AllowCredentials: true})
+	return coresCfg.Handler(handler)
+}
+
+func handleRateLimiting(next http.Handler, cfg config.RateLimiting) http.Handler {
 	// Sip rate limiter when disabled
 	if !cfg.Enabled {
 		return next
@@ -36,39 +85,4 @@ func initRateLimter(next http.Handler, cfg config.RateLimiting) http.Handler {
 	limit.SetMessageContentType("application/json")
 
 	return tollbooth.LimitHandler(limit, next)
-}
-
-func initLogging(cfg config.Configuration) {
-	level, err := log.ParseLevel(string(cfg.Log.Level))
-	if err != nil {
-		log.Fatalf("Invalid logrus level: %v", err)
-	}
-	log.SetOutput(os.Stdout)
-	log.SetLevel(level)
-	log.Info("Log level set to: ", log.GetLevel())
-}
-
-func main() {
-	cfg := config.NewConfig()
-	initLogging(cfg)
-	appMetrics := metrics.CreateMetrics()
-
-	backend := backends.NewBackend(cfg.Backend)
-	if cfg.RequestLimits.MaxSize > 0 {
-		backend = backendDecorators.EnforceSizeLimit(backend, cfg.RequestLimits.MaxSize)
-	}
-	backend = backendDecorators.LogMetrics(backend, appMetrics)
-	if viper.GetString("compression.type") == "snappy" {
-		backend = compression.SnappyCompress(backend)
-	}
-
-	router := httprouter.New()
-	router.GET("/status", endpoints.Status) // Determines whether the server is ready for more traffic.
-
-	router.POST("/cache", endpointDecorators.MonitorHttp(endpoints.NewPutHandler(backend, cfg.RequestLimits.MaxNumValues), appMetrics.Puts))
-	router.GET("/cache", endpointDecorators.MonitorHttp(endpoints.NewGetHandler(backend), appMetrics.Gets))
-
-	go appMetrics.Export(cfg.Metrics)
-
-	server.Listen(cfg, router, appMetrics.Connections)
 }
