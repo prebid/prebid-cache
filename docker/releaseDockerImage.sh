addf3c59483a95127162a18d1cbb830426afe5f9
Shalmali Patil <shalmali.patil@pubmatic.com>
2018-07-25 19:48:33 +0530

    Adding placeholders for Jenkins job

--- docker/releaseDockerImage.sh
+++ docker/releaseDockerImage.sh
@@ -5,6 +5,7 @@
 
 
 sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD -e $DOCKER_EMAIL  docker.pubmatic.com
+echo $DOCKER_USERNAME : $DOCKER_PASSWORD : $DOCKER_EMAIL
 echo $GROUP_ID : $ARTIFACT_ID : $ARTIFACT_VERSION : $FINAL_ARTIFACT_NAME : $ARTIFACT_PACKAGING_TYPE ;
 
 #Copy the template docker file to actual docker file which will be used for current build.
