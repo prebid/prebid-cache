7b506fb4ec59497aa28aac8987e44623ac63ec9a
UOE Dev <UOEDev@pubmatic.com>
2018-07-26 17:45:08 +0530

    committing changes for docker

--- docker/releaseDockerImage.sh
+++ docker/releaseDockerImage.sh
@@ -6,7 +6,7 @@
 mkdir -p cache
 mkdir -p cache/app-resources
 cp -rf ../prebid-cache ./cache/
-cp -rf ../../../../applications-k8s-config/prebid-cache/docker/* ./cache/app-resources/
+cp -rf ../../../../applications-k8s-config.git/prebid-cache/docker/* ./cache/app-resources/
 
 #Copy the template docker file to actual docker file which will be used for current build.
 rm -rf Dockerfile
