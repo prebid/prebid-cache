0d2941dca4ea06031cf2086f413a2ed3af255f3c
Hoverr Hacker <venkatesh@hoverr.co>
2018-12-11 19:28:57 +0530

    Changed Default index message

--- endpoints/index.go
+++ endpoints/index.go
@@ -3,11 +3,14 @@ package endpoints
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
+//Handle Default route for the prebid-cache
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	//Default routing instead of showing 404 not found error
 	// Added Default routing handler added message instead of trowing 404 Error
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf("Prebid Cache Index Route.")
+	fmt.Fprintf(w, "This application stores short-term data for use in Prebid.")
 }
