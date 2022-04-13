93f018f2f15355ce9dc11946386df85913bb24e4
guscarreon <guscarreon@gmail.com>
2022-04-13 13:18:31 -0400

    Zero TTL fix (#107)
    * Defined TTL constants and wrote decorator test case
    
    * Decorator logic modified and asserted in test case
    
    * Added test coverage to backends/config/config.go
    
    * Brian's review
    
    * Corrected Aerospike error message and assert.Contains
    
    Co-authored-by: Gus Carreon <gcarreongutierrez@vpn-10-249-98-20.nym2.appnexus.com>

--- backends/constants.go
+++
@@ -1,7 +0,0 @@
-package backends
-
-// These strings are prefixed onto data put in the backend, to designate its type.
-const (
-	XML_PREFIX  = "xml"
-	JSON_PREFIX = "json"
-)
--- backends/decorators/limit_ttls.go
+++ backends/decorators/limit_ttls.go
@@ -4,24 +4,41 @@ import (
 	"context"
 
 	"github.com/prebid/prebid-cache/backends"
+	"github.com/prebid/prebid-cache/utils"
 )
 
 // LimitTTLs wraps the delegate and makes sure that it never gets TTLs which exceed the max.
+// or are less than zero.
 func LimitTTLs(delegate backends.Backend, maxTTLSeconds int) backends.Backend {
+	maxTTL := maxTTLSeconds
+	if maxTTLSeconds <= 0 {
+		maxTTL = utils.REQUEST_MAX_TTL_SECONDS
+	}
 	return ttlLimited{
 		Backend:       delegate,
-		maxTTLSeconds: maxTTLSeconds,
+		maxTTLSeconds: maxTTL,
 	}
 }
 
+// ttlLimited implements the backends.Backend interface to serve as a decorator that enforces
+// a time-to-live limit on incoming PUT requests.
 type ttlLimited struct {
 	backends.Backend
 	maxTTLSeconds int
 }
 
-func (l ttlLimited) Put(ctx context.Context, key string, value string, ttlSeconds int) error {
-	if l.maxTTLSeconds > ttlSeconds {
-		return l.Backend.Put(ctx, key, value, ttlSeconds)
+// Put will make the delegate.Put() call with the default l.maxTTLSeconds whenever the
+// request-defined ttl value is out of bounds
+func (l ttlLimited) Put(ctx context.Context, key string, value string, requestTTLSeconds int) error {
+	ttl := l.maxTTLSeconds
+
+	if l.maxTTLSeconds > requestTTLSeconds && requestTTLSeconds > 0 {
+		ttl = requestTTLSeconds
 	}
-	return l.Backend.Put(ctx, key, value, l.maxTTLSeconds)
+	return l.Backend.Put(ctx, key, value, ttl)
+}
+
+// Get will somply make the delegate.Get() call given that no TTL check is needed on the GET side
+func (l ttlLimited) Get(ctx context.Context, key string) (string, error) {
+	return l.Backend.Get(ctx, key)
 }
