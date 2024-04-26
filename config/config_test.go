9bca40c8781e79fb80837a9b256c2ec1c76fae98
Sébastien Millet <sebastien.millet@dailymotion.com>
2024-04-26 11:59:09 +0200

    Fix configuration test and remove unused expiration field

--- config/config_test.go
+++ config/config_test.go
@@ -1277,6 +1277,16 @@ func getExpectedFullConfigForTestFile() Configuration {
 					InsecureSkipVerify: false,
 				},
 			},
+			RedisSentinel: RedisSentinel{
+				SentinelAddrs: []string{"127.0.0.1:26379", "127.0.0.1:26380", "127.0.0.1:26381"},
+				MasterName:    "mymaster",
+				Password:      "",
+				Db:            1,
+				TLS: RedisTLS{
+					Enabled:            false,
+					InsecureSkipVerify: false,
+				},
+			},
 			Ignite: Ignite{
 				Scheme: "http",
 				Host:   "127.0.0.1",
