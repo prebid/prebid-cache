3a54234971f88303505e1a534b883a3b17961868
Kiril Kalchev <kiril.kalchev@gmail.com>
2025-08-05 18:06:57 +0300

    finalize redis cluster implementation

--- backends/redis.go
+++ backends/redis.go
@@ -12,16 +12,56 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// RedisDB is an interface that helps us communicate with an instance of a
+// Redis database. Its implementation is intended to use the "github.com/go-redis/redis"
+// client
+type RedisDB interface {
+	Get(ctx context.Context, key string) (string, error)
+	Put(ctx context.Context, key string, value string, ttlSeconds int) (bool, error)
+}
+
+// RedisDBClient is a wrapper for the Redis client that implements
+// the RedisDB interface. It can work with both standalone and cluster clients.
+type RedisDBClient struct {
+	client interface{} // Can be either *redis.Client or *redis.ClusterClient
+}
+
+// Get returns the value associated with the provided `key` parameter
+func (db RedisDBClient) Get(ctx context.Context, key string) (string, error) {
+	switch c := db.client.(type) {
+	case *redis.Client:
+		return c.Get(ctx, key).Result()
+	case *redis.ClusterClient:
+		return c.Get(ctx, key).Result()
+	default:
+		return "", utils.NewPBCError(utils.KEY_NOT_FOUND)
+	}
+}
+
+// Put will set 'key' to hold string 'value' if 'key' does not exist in the redis storage.
+// When key already holds a value, no operation is performed. That's the reason this adapter
+// uses the 'github.com/go-redis/redis's library SetNX. SetNX is short for "SET if Not eXists".
+func (db RedisDBClient) Put(ctx context.Context, key, value string, ttlSeconds int) (bool, error) {
+	switch c := db.client.(type) {
+	case *redis.Client:
+		return c.SetNX(ctx, key, value, time.Duration(ttlSeconds)*time.Second).Result()
+	case *redis.ClusterClient:
+		return c.SetNX(ctx, key, value, time.Duration(ttlSeconds)*time.Second).Result()
+	default:
+		return false, utils.NewPBCError(utils.KEY_NOT_FOUND)
+	}
+}
+
 // RedisBackend when initialized will instantiate and configure the Redis client. It implements
 // the Backend interface.
 type RedisBackend struct {
 	cfg    config.Redis
-	client redis.Cmdable // This interface is implemented by both redis.Client and redis.ClusterClient
+	client RedisDB
 }
 
 // NewRedisBackend initializes the redis client and pings to make sure connection was successful
 func NewRedisBackend(cfg config.Redis, ctx context.Context) *RedisBackend {
-	var client redis.Cmdable
+	var redisClient RedisDBClient
 
 	if cfg.Cluster.Enabled {
 		// Cluster mode
@@ -47,7 +87,7 @@ func NewRedisBackend(cfg config.Redis, ctx context.Context) *RedisBackend {
 		}
 
 		log.Infof("Connected to Redis cluster with hosts: %v", cfg.Cluster.Hosts)
-		client = clusterClient
+		redisClient = RedisDBClient{client: clusterClient}
 	} else {
 		// Single-node mode
 		constr := cfg.Host + ":" + strconv.Itoa(cfg.Port)
@@ -74,12 +114,12 @@ func NewRedisBackend(cfg config.Redis, ctx context.Context) *RedisBackend {
 		}
 
 		log.Infof("Connected to Redis at %s:%d", cfg.Host, cfg.Port)
-		client = singleClient
+		redisClient = RedisDBClient{client: singleClient}
 	}
 
 	return &RedisBackend{
 		cfg:    cfg,
-		client: client,
+		client: redisClient,
 	}
 }
 
@@ -87,7 +127,7 @@ func NewRedisBackend(cfg config.Redis, ctx context.Context) *RedisBackend {
 // parameter and interprets its response. A `Nil` error reply of the Redis client means
 // the `key` does not exist.
 func (b *RedisBackend) Get(ctx context.Context, key string) (string, error) {
-	res, err := b.client.Get(ctx, key).Result()
+	res, err := b.client.Get(ctx, key)
 
 	if err == redis.Nil {
 		err = utils.NewPBCError(utils.KEY_NOT_FOUND)
@@ -101,7 +141,7 @@ func (b *RedisBackend) Get(ctx context.Context, key string) (string, error) {
 // not being written because the `key` already holds a value, and a RecordExistsError is returned
 func (b *RedisBackend) Put(ctx context.Context, key string, value string, ttlSeconds int) error {
 
-	success, err := b.client.SetNX(ctx, key, value, time.Duration(ttlSeconds)*time.Second).Result()
+	success, err := b.client.Put(ctx, key, value, ttlSeconds)
 	if err != nil && err != redis.Nil {
 		return err
 	}
